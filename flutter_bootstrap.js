e43b60593bd3d8b1f6bddf6bbce00acb7057ed06
zeyus <zeyus@users.noreply.github.com>
2025-05-01 07:51:56 +0000

    deploy: 18306aac4d5ccff7c64d841b110f6f4350ccfe70

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4146314188"
+    serviceWorkerVersion: "3360282374"
   }
 });
